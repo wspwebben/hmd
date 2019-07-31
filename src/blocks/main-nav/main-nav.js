fa7b604f4b6dbea29a4fa3f1019d97a2d3f4aeb4
webben <wspwebben@gmail.com>
2019-07-31 12:40:30 +0700

    fixed max-height collapse

--- src/blocks/main-nav/main-nav.js
+++ src/blocks/main-nav/main-nav.js
@@ -2,8 +2,12 @@ const menu = document.querySelector('.main-nav');
 const menuToggle = menu.querySelector('.main-nav__toggle');
 const menuWrapper = menu.querySelector('.main-nav__wrapper');
 
+const openClass = 'main-nav__wrapper--open';
+
+menuWrapper.classList.add(openClass);
 menuWrapper.style.maxHeight = `${menuWrapper.clientHeight}px`;
+menuWrapper.classList.remove(openClass);
 
 menuToggle.addEventListener('click', function() {
-  menuWrapper.classList.toggle('main-nav__wrapper--open')
+  menuWrapper.classList.toggle(openClass)
 });

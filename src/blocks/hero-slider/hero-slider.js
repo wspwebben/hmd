0847ddf0fdbda133008a619159091ba410c165f9
webben <wspwebben@gmail.com>
2019-10-07 21:32:51 +0700

    extracted const

--- src/blocks/hero-slider/hero-slider.js
+++ src/blocks/hero-slider/hero-slider.js
@@ -1,11 +1,12 @@
 import Swiper from 'swiper';
 
+import { GUTTER } from './../../js/consts';
+
 const block = 'js-hero-slider';
-const gutter = 20;
 
 const slider = new Swiper(`.${block}__slider`, {
   loop: true,
-  spaceBetween: gutter,
+  spaceBetween: GUTTER,
   navigation: {
     prevEl: `.${block}__prev`,
     nextEl: `.${block}__next`,

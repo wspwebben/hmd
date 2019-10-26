08ff5cc0dd5ffb621ec142ff5533d87e62a6a4d5
webben <wspwebben@gmail.com>
2019-10-26 21:04:44 +0700

    enhanced testimonials with swiper controllers no-js version allows to scroll through reviews

---
+++ src/blocks/testimonials/testimonials.js
@@ -0,0 +1,22 @@
+import Swiper from 'swiper';
+
+import { GUTTER } from './../../js/consts';
+
+const block = 'js-testimonials';
+
+const mainSlider = new Swiper(`.${block}__main-slider`, {
+  loop: true,
+  spaceBetween: GUTTER,
+  navigation: {
+    prevEl: `.${block}__prev`,
+    nextEl: `.${block}__next`,
+  },
+});
+
+const subSlider = new Swiper(`.${block}__sub-slider`, {
+  loop: true,
+  spaceBetween: GUTTER,
+});
+
+subSlider.controller.control = mainSlider;
+mainSlider.controller.control = subSlider;

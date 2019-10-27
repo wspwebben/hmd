c1589b13d1c97278558865728a178ce00add5392
webben <wspwebben@gmail.com>
2019-10-27 15:26:26 +0700

    fixed slides counter in hero sliderfixed inline slider scrollbar

--- src/blocks/hero-slider/hero-slider.js
+++ src/blocks/hero-slider/hero-slider.js
@@ -3,12 +3,24 @@ import Swiper from 'swiper';
 import { GUTTER } from './../../js/consts';
 
 const block = 'js-hero-slider';
+const elem = document.querySelector(`.${block}__slider`);
+const progress = elem.querySelector(`.${block}__progress`);
 
-const slider = new Swiper(`.${block}__slider`, {
+const makePrefix = index => `${index < 10 ? '0' : ''}${index}`;
+
+const slider = new Swiper(elem, {
   loop: true,
   spaceBetween: GUTTER,
   navigation: {
     prevEl: `.${block}__prev`,
     nextEl: `.${block}__next`,
   },
+  on: {
+    init() {
+      progress.textContent = makePrefix(this.realIndex + 1);
+    },
+    slideChange() {
+      progress.textContent = makePrefix(this.realIndex + 1);
+    }
+  }
 });

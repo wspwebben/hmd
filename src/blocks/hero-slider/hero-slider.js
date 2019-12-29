92c96bddc2ac8a37915320ee542873baeeacd27c
webben <wspwebben@gmail.com>
2019-12-30 00:25:14 +0700

    rewrote main slider for mobile/tablet

--- src/blocks/hero-slider/hero-slider.js
+++ src/blocks/hero-slider/hero-slider.js
@@ -3,27 +3,45 @@ import Swiper from 'swiper';
 import { GUTTER } from './../../js/consts';
 
 const block = 'js-hero-slider';
-const elem = document.querySelector(`.${block}__slider`);
+const blockElement = document.querySelector(`.${block}`);
+const imageWrapper = blockElement.querySelector(`.${block}__image-slider`);
+const contentWrapper = blockElement.querySelector(`.${block}__content-slider`);
 
-if (elem) {
-  const progress = elem.querySelector(`.${block}__progress`);
+const padIndex = index => String(index).padStart(2, '0');
 
-  const makePrefix = index => `${index < 10 ? '0' : ''}${index}`;
+if (imageWrapper && contentWrapper) {
+  const progress = blockElement.querySelector(`.${block}__progress`);
 
-  const slider = new Swiper(elem, {
+  const updateProgress = index => {
+    // first slide has 0 index
+    const friendlyIndex = index + 1;
+    progress.textContent = padIndex(friendlyIndex);
+  }
+
+  const imageSlider = new Swiper(imageWrapper, {
     loop: true,
     spaceBetween: GUTTER,
     navigation: {
       prevEl: `.${block}__prev`,
       nextEl: `.${block}__next`,
     },
+    effect: 'coverflow',
+    speed: 500,
     on: {
       init() {
-        progress.textContent = makePrefix(this.realIndex + 1);
+        updateProgress(this.realIndex);
       },
       slideChange() {
-        progress.textContent = makePrefix(this.realIndex + 1);
+        updateProgress(this.realIndex); 
       }
     }
   });
+
+  const contentSlider = new Swiper(contentWrapper, {
+    loop: true,
+    spaceBetween: GUTTER,
+  });
+
+  contentSlider.controller.control = imageSlider;
+  imageSlider.controller.control = contentSlider;
 }

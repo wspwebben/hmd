5c1d05f209c1763656eacf7165ed3b8fbaa3553c
webben <wspwebben@gmail.com>
2020-01-04 23:39:48 +0700

    fixed element selecting

--- src/blocks/hero-slider/hero-slider.js
+++ src/blocks/hero-slider/hero-slider.js
@@ -4,47 +4,50 @@ import { GUTTER } from './../../js/consts';
 
 const block = 'js-hero-slider';
 const blockElement = document.querySelector(`.${block}`);
-const imageWrapper = blockElement.querySelector(`.${block}__image-slider`);
-const contentWrapper = blockElement.querySelector(`.${block}__content-slider`);
 
-const padIndex = index => String(index).padStart(2, '0');
+if (blockElement) {
+  const imageWrapper = blockElement.querySelector(`.${block}__image-slider`);
+  const contentWrapper = blockElement.querySelector(`.${block}__content-slider`);
 
-if (imageWrapper && contentWrapper) {
-  const progress = blockElement.querySelector(`.${block}__progress`);
+  const padIndex = index => String(index).padStart(2, '0');
 
-  const updateProgress = index => {
-    // first slide has 0 index
-    const friendlyIndex = index + 1;
-    progress.textContent = padIndex(friendlyIndex);
-  }
+  if (imageWrapper && contentWrapper) {
+    const progress = blockElement.querySelector(`.${block}__progress`);
 
-  const imageSlider = new Swiper(imageWrapper, {
-    loop: true,
-    spaceBetween: GUTTER,
-    navigation: {
-      prevEl: `.${block}__prev`,
-      nextEl: `.${block}__next`,
-    },
-    effect: 'coverflow',
-    coverflowEffect: {
-      slideShadows: false,
-    },
-    speed: 500,
-    on: {
-      init() {
-        updateProgress(this.realIndex);
-      },
-      slideChange() {
-        updateProgress(this.realIndex); 
-      }
+    const updateProgress = index => {
+      // first slide has 0 index
+      const friendlyIndex = index + 1;
+      progress.textContent = padIndex(friendlyIndex);
     }
-  });
 
-  const contentSlider = new Swiper(contentWrapper, {
-    loop: true,
-    spaceBetween: GUTTER,
-  });
+    const imageSlider = new Swiper(imageWrapper, {
+      loop: true,
+      spaceBetween: GUTTER,
+      navigation: {
+        prevEl: `.${block}__prev`,
+        nextEl: `.${block}__next`,
+      },
+      effect: 'coverflow',
+      coverflowEffect: {
+        slideShadows: false,
+      },
+      speed: 500,
+      on: {
+        init() {
+          updateProgress(this.realIndex);
+        },
+        slideChange() {
+          updateProgress(this.realIndex); 
+        }
+      }
+    });
 
-  contentSlider.controller.control = imageSlider;
-  imageSlider.controller.control = contentSlider;
+    const contentSlider = new Swiper(contentWrapper, {
+      loop: true,
+      spaceBetween: GUTTER,
+    });
+
+    contentSlider.controller.control = imageSlider;
+    imageSlider.controller.control = contentSlider;
+  }
 }

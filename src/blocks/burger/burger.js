eed034282bbc09110427274d9d714090db60123c
wspwebben <wspwebben@gmail.com>
2020-11-17 23:35:08 +0700

    download wordpress

--- src/blocks/burger/burger.js
+++
@@ -1,7 +0,0 @@
-const burgers = document.querySelectorAll('.js-burger');
-
-burgers.forEach(burger => {
-  burger.addEventListener('click', function() {
-    this.classList.toggle('js-burger--active');
-  });
-});
--- src/blocks/modal/modal.js
+++
@@ -1,3 +0,0 @@
-import Micromodal from 'micromodal';
-
-Micromodal.init();
